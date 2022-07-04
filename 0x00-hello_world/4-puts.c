aa37e6048bc35c8a846757c3c17f412e3341791a
tamirats <tamirtadesse19@gmail.com>
2022-07-04 12:35:08 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+Include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return: 0 after printing the function
+ */
+Int main(void)
+{puts(“\” Programming is like building a multilingual puzzle”);
+	return (0);
+}
+
